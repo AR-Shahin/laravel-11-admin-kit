1279f2cd876743ecff6fe66435766f2141c8c175
AR-Shahin <mdshahinmije96@gmail.com>
2024-03-16 17:34:34 +0600

    up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,8 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-
+        <link rel="stylesheet" href="{{ asset("build/assets/app-CfZdtZXK.css") }}">
+        <script src="{{ asset("build/assets/app-BgvOogpt.js") }}"></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
